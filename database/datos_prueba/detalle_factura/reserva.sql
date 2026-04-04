6ff73c037d59b83909be04bc4c8012dacf274e00
Dreathward <JhonAPL14@outlook.com>
2026-04-04 16:40:39 -0500

    Correción de reserva.sql

--- database/datos_prueba/detalle_factura/reserva.sql
+++ database/datos_prueba/detalle_factura/reserva.sql
@@ -1,4 +1,4 @@
 INSERT INTO detalle_facturas (id_factura, concepto, monto)
-SELECT f.id_factura, 'multa', 20000
+SELECT f.id_factura, 'reserva', 50000
 FROM facturas f
-WHERE f.estado = 'vencido';
+WHERE MOD(f.id_factura, 7) = 0;
